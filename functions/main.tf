c29e15677cac3ae61533d670391352e4a7365409
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-09-25 20:18:02 +0530

    Variables in terraform - functions

--- functions/main.tf
+++ functions/main.tf
@@ -4,6 +4,7 @@ output "apple" {
   value = "Apple quantity is ${var.fruits["apple"]["quantity"]} and price is ${var.fruits["apple"]["price"]}"
 }
 
+#access variable which is not present in tfvars file
 output "banana" {
-  value = "Banana quantity is ${var.fruits["banana"]["quantity"]}"
+  value = "Banana quantity is ${var.fruits["banana"]["quantity"]} and price is ${lookup(var.fruits["banana"],"price", 0.5)}"
 }
