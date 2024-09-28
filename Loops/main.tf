56d6a8c91781db53f5b180be7e36fc141bc67b5a
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-09-28 14:52:22 +0530

    Variables in terraform - functions

---
+++ Loops/main.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "test" {
+  count = length(var.components)
+
+}
+
+variable "components" {
+  fruits = [ apple, banana ]
+}
+
+output "fruits" {
+  count = length(var.components)
+  value = var.components[count.index]
+}
